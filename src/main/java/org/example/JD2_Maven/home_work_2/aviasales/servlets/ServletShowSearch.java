28a0c015f646671ea2331522298006bf03b020e4
CharismaticGun <95929665+CharismaticGun@users.noreply.github.com>
2022-05-05 10:05:08 +0300

    few_changes

--- src/main/java/org/example/JD2_Maven/home_work_2/aviasales/servlets/ServletShowSearch.java
+++ src/main/java/org/example/JD2_Maven/home_work_2/aviasales/servlets/ServletShowSearch.java
@@ -2,6 +2,7 @@ package org.example.JD2_Maven.home_work_2.aviasales.servlets;
 
 import org.example.JD2_Maven.home_work_2.aviasales.dao_airports.AirportPullDao;
 import org.example.JD2_Maven.home_work_2.aviasales.dao_airports.api.IAirportDao;
+import org.example.JD2_Maven.home_work_2.aviasales.service.FlightsService;
 
 import javax.servlet.*;
 import javax.servlet.http.*;
@@ -13,12 +14,16 @@ public class ServletShowSearch extends HttpServlet {
 
     private final IAirportDao dao = new AirportPullDao();
 
+    private final FlightsService service = FlightsService.getInstance();
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
         ServletContext context = request.getServletContext();
 
         request.setAttribute("airports",dao.getAll());
+
+        service.getSb().setLength(0);
 
         context.getRequestDispatcher("/jsp/aviasales/race_form.jsp").forward(request,response);
     }
